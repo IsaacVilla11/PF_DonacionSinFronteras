c4eed253382cadf4e3953ad17bbbd3d65986fe98
IsaacVilla11 <isaac.villa.est@tecazuay.edu.ec>
2024-01-10 22:29:22 -0500

    Inicio proyecto

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import Vista.V_Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        V_Principal vista = new V_Principal();
+        vista.setVisible(true);
+    }
+}
