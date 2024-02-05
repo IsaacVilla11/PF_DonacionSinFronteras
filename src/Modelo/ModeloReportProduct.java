4c7d2b6717994e39d403424bd33a68601c23872b
Bfarez21 <bryanfareznieves@gmail.com>
2024-02-05 17:58:28 -0500

    Modelo Report Producto, metod para mostrar solo registros correspondiente a un ID

--- src/Modelo/ModeloReportProduct.java
+++ src/Modelo/ModeloReportProduct.java
@@ -18,27 +18,37 @@ import java.util.List;
  */
 public class ModeloReportProduct {
 
-    public List<EncabezadoFactura> obtenerReport() {
-        List<EncabezadoFactura> detalles = new ArrayList<>();
+public List<EncabezadoFactura> obtenerReport(int idPersona) {
+    List<EncabezadoFactura> detalles = new ArrayList<>();
 
-        try ( Connection connection = new ConexionPg().getCon()) {
-            String sql = "SELECT id_encabezadofact, fecha_fact, total FROM encabezadofactura";
+    try (Connection connection = new ConexionPg().getCon()) {
+        // Corrige la sintaxis en la cláusula WHERE
+        String sql = "SELECT e.id_encabezadofact, e.fecha_fact, d.cant_produ_det, e.total\n" +
+                     "FROM encabezadofactura e\n" +
+                     "JOIN detallefactura d ON e.id_encabezadofact = d.id_detallefact\n" +
+                     "WHERE e.id_comprador_fact = ?";
 
-            try ( PreparedStatement statement = connection.prepareStatement(sql);  ResultSet resultSet = statement.executeQuery()) {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            // Asigna el valor del parámetro idPersona al PreparedStatement
+            statement.setInt(1, idPersona);
 
+            try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
                     EncabezadoFactura enca = new EncabezadoFactura();
                     enca.setId_encabezadoFact(resultSet.getInt("id_encabezadofact"));
                     enca.setFecha_fact(resultSet.getDate("fecha_fact"));
+                   
                     enca.setTotal(resultSet.getDouble("total"));
 
                     detalles.add(enca);
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace(); // Manejar la excepción adecuadamente en tu aplicación
         }
-
-        return detalles;
+    } catch (SQLException e) {
+        e.printStackTrace(); // Manejar la excepción adecuadamente en tu aplicación
     }
+
+    return detalles;
+}
+
 }
